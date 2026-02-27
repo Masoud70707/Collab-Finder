570c997ff196189a6f8ce4f8a8d9c99e8b5d5b51
Masoud Zhianmanesh <masoud.zhianmanesh@sydney.edu.au>
2026-02-27 12:31:43 +1100

    fix: use render PORT env

--- app.py
+++ app.py
@@ -1269,4 +1269,5 @@ if __name__ == "__main__":
     else:
         init_db()
 
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
